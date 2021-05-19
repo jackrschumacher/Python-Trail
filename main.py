0af1bc21c7349a908ab39c26e99ef9aca13c1ee7
Jack Schumacher <>
2021-05-19 22:48:03 +0000

    intro

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def intro():
   print("Your name is,",name)
   age = str(input("Please enter your age:"))
   print("Your age is:",age)
-
+  location_start = str(input("Where would you like to start your adventure from?"))
+  print("You have chosen to start in:",location_start)
 
 print("Welcome to Python Trail")
 intro()
