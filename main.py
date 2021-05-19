372b590bccdf080dd242105d72ed37efb4c59951
Jack Schumacher <>
2021-05-19 22:41:35 +0000

    Setup and intro()

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 print("Welcome to Python Trail")
+intro()
 
+
+def intro():
+  name = str(input("What is your name?"))
+  print("Your name is,"name)
+  age = str(input("Please enter your age:"))
+  print("Your age is:",age)
 
 
 
