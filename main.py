fccdd7ce7ddd3bef7ec3aa13c6d07ee31a28b2d0
Jack Schumacher <>
2021-05-19 22:43:43 +0000

    Formatting

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+def intro():
+  name = str(input("What is your name?"))
+  print("Your name is,",name)
+  age = str(input("Please enter your age:"))
+  print("Your age is:",age)
+
+
 print("Welcome to Python Trail")
 intro()
 
 
-def intro():
-  name = str(input("What is your name?"))
-  print("Your name is,"name)
-  age = str(input("Please enter your age:"))
-  print("Your age is:",age)
+
+
 
 
 
