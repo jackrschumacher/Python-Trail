a3bd981663e4d3b9e716252ff15a4411f1af2ea5
Jack Schumacher <>
2021-05-19 23:49:47 +0000

    Animal Amount/Type added

--- main.py
+++ main.py
@@ -15,12 +15,17 @@ def game_varsetup():
   print("You have $400 dollars to spend")
   amnt_food = int(input("Please enter the amount of food you wish to have(food costs $10 per pound):"))
   print("You have chosen to buy", amnt_food,"Pounds of food")
-  dollars = dollars-amnt_food*10
+  dollars = dollars-amnt_food * 10
   print("You have,",dollars,"dollars remaining")
   amnt_water = int(input("Please enter the amount of water you wish to carry(Water 1 Dollar per gallon):"))
-  dollars = dollars-amnt_water*2
+  dollars = dollars-amnt_water * 2
   print("You have,",dollars,"dollars remaining")
-  amnt_horses 
+  animal = ("Please enter the animal you would like to pull your wagon:")
+  print("You Have Chosen:",animal,"To pull your wagon")
+  animal_amnt = int(input("Please enter the number of animals you wish to pull your wagon (animals are 15 per animal): "))  
+  print("You purchased:",animal_amnt,"Animals")
+  dollars = dollars-animal_amnt
+  print("You have:",dollars,"Dollars Remaining")
 
 
 
