57eb4d6ad7fa688ecaf2c1835cc0cb5c1766a476
Jack Schumacher <>
2021-05-19 22:58:18 +0000

    Added game setup

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ def intro():
   print("You have chosen to start in:",location_start)
   location_end = str(input("Where would you like your adventure to end?"))
   print("You have chosen to end at:",location_end)
-  
+
+def game_varsetup():
+  dollars = 400
+  print("You have $400 dollars to spend")
+  amnt_food = str(input("Please enter the amount of food you wish to have(food costs $10 per pound):"))
+  print("You have chose ")
+
+
+
 print("Welcome to Python Trail")
 intro()
 
