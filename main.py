4e2387d1d679ef7354a7517ab248c18431c630f3
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2021-06-11 21:29:42 +0000

    Fix Typos, added Wheel amount variable, fixed dollar variable for axle

--- main.py
+++ main.py
@@ -59,9 +59,13 @@ def game_varsetup():     #Buy Supplies
   dollars = dollars-animal_amnt
   print("You have,",dollars,"PyCoin Remaining")
   axle_amnt = int(input("Please enter the number of axles you would like to purchase:"))
-  print("You have bought:",axke_amnt)
-  dollars = dollars *3
-  print("You have:",dollars,"Remaining")
+  print("You have bought:",axle_amnt)
+  dollars = dollars - axle_amnt *2
+  print("You have:",dollars,"Pycoin Remaining")
+  wheel_amnt = int(input("Please enter the number of wheels you would like to purchase"))
+  print("You have bought:",wheel_amnt,"wheels")
+  dollars = dollars - wheel_amnt *3
+  print("You have,"dollars,"Pycoin Remaining")
   medicine_amnt = int(input("Please enter the amount of medicine you wish to buy ($10)"))
   print("You have purchased,",medicine_amnt,"Medicine")
   dollars = dollars - medicine_amnt * 10
